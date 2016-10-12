414cbf262bdbc882eb4fd0e273433bb3f4ca6c27
Hokyung <ghrud92@gmail.com>
2016-10-12 16:03:11 +0900

    main activity 추가

---
+++ app/src/main/java/com/estsoft/memosquare/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.estsoft.memosquare.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.estsoft.memosquare.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
