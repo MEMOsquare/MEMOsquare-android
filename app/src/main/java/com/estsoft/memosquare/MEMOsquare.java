c0dbc891bcfe8729683922070c3514ba83118a9f
Hokyung <ghrud92@gmail.com>
2016-10-13 14:16:57 +0900

    디버깅과 릴리즈시의 로그 분리

--- app/src/main/java/com/estsoft/memosquare/MEMOsquare.java
+++ app/src/main/java/com/estsoft/memosquare/MEMOsquare.java
@@ -1,6 +1,7 @@
 package com.estsoft.memosquare;
 
 import android.app.Application;
+import android.util.Log;
 
 import timber.log.Timber;
 
@@ -18,6 +19,32 @@ public class MEMOsquare extends Application {
             Timber.plant(new Timber.DebugTree());
         } else {
             // Release 버전에서의 로그 트리
+            Timber.plant(new ReleaseTree());
+        }
+    }
+
+    // 상용 버전에서의 로그
+    private static class ReleaseTree extends Timber.Tree {
+
+        @Override
+        protected void log(int priority, String tag, String message, Throwable t) {
+
+            // 디버깅용 로그는 나타내지 않는다
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return;
+            }
+
+            // INFO 로그의 경우
+            Log.i(tag, message);
+
+            // ERROR와 WARNING
+            if (t != null) {
+                if (priority == Log.ERROR) {
+                    Log.e(tag, message, t);
+                } else if (priority == Log.WARN) {
+                    Log.w(tag, message, t);
+                }
+            }
         }
     }
 }
